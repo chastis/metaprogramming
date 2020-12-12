7b2a432713ed99f42f3bcaf348ce882cf79cbfe0
lipskydan <lipsky@knu.ua>
2020-12-12 22:15:15 +0200

    Created struct of cmdline app

---
+++ Py2SQL-cmdline-app/py2sql-runner.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""Convenience wrapper for running bootstrap directly from source tree."""
+
+from Py2SQL.py2sql import main
+
+if __name__ == '__main__':
+    main()
