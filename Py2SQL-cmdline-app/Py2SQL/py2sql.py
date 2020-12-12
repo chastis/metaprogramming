a351d766048d32ec54ca343e4a905173858d026f
lipskydan <lipsky@knu.ua>
2020-12-12 23:48:45 +0200

    +

--- Py2SQL-cmdline-app/Py2SQL/py2sql.py
+++ Py2SQL-cmdline-app/Py2SQL/py2sql.py
@@ -5,9 +5,6 @@
 
 __version__ = "0.2.0"
 
-import sys
-
 
 def main():
-    print("Executing bootstrap version %s." % __version__)
-    print("List of argument strings: %s" % sys.argv[1:])
+    print("Executing Py2SQL version %s." % __version__)
