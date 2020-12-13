a917a0d6aaf634b3053ceaf4ba3d848c5de5fda5
marynakukhta <marynakukhta@users.noreply.github.com>
2020-12-13 22:34:22 +0200

    Fixed some changes

--- Py2SQL-cmdline-app/Py2SQL/py2sql.py
+++ Py2SQL-cmdline-app/Py2SQL/py2sql.py
@@ -17,16 +17,126 @@ from Py2SQL import sql_queries
 
 
 class Car:
-    # def __init__(self, color: str = 'RED', number: int = 5, title: str = "BMW"):
-    #     self.color = color
-    #     self.number = number
-    #     self.title = title
+    def __init__(self, color: str = 'RED', number: int = 5, title: str = "BMW"):
+        self.color = color
+        self.number = number
+        self.title = title
     color = str
     number = int
     title = str
 
+class Cat:
+    def __init__(self, id_=1, name='Барсик'):
+        self.id = id_
+        self.name = name
+    id = 1
+    name = 'барсик'
 
 class Database:
+
+    def save_object(self, py_object: object):
+        """
+            Сохранение объекта в базу данных
+        :param py_object: объект
+        :return: статус добавления, или же контролируемая ошибка
+        """
+        need_fields: List[Tuple] = handler.convert_python_types_in_sqlite_types(attributes=tuple(vars(py_object).items()),
+                                                                                object_=True)
+        tables_list: tuple = self.get_tables()
+
+        for table in tables_list:
+
+            if set(row[1:] for row in self.get_table_info(table)) == set(need_fields):
+
+                try:
+                    self._execute(queries=util.check_having_in_table(table=table,
+                                                                     id_=py_object.id))
+                    update: bool = bool(self._cursor.fetchone())
+                except AttributeError:
+                    raise Exception(EXCEPTION_TEXT_NO_ID)
+
+                self._commit(queries=util.generate_save_sql_query(table=table,
+                                                                  py_object=py_object,
+                                                                  update=update))
+                return RECORD_UPDATE if update else RECORD_INSERT
+
+        else:
+            raise Exception(EXCEPTION_TEXT_NO_TABLE_WITH_THESE_ATTRS)
+
+    def save_class(self, py_class):
+        """
+            Сохранение таблицы в БД
+        :param py_class: класс, по которому будет выстроена бд
+        :return: статус сохранения или контролируемая ошибка
+        """
+        need_fields: List[Tuple] = handler.convert_python_types_in_sqlite_types(attributes=tuple(vars(py_class).items()),
+                                                                                object_=True)
+        tables_list: tuple = self.get_tables()
+        title_class: str = py_class.__name__.lower()
+        if title_class not in tables_list:
+            self._commit(queries=util.create_table(title=title_class,
+                                                   attributes=need_fields))
+            return TABLE_WAS_CREATED
+        else:
+            for table in tables_list:
+                if set(row[1:] for row in self.get_table_info(table)) == set(need_fields):
+                    raise Exception(EXCEPTION_TEXT_CLASS_EXIST)
+            else:
+                self._execute(sql_queries.DROP_TABLE(title_class))
+                self._commit(queries=util.create_table(title=title_class,
+                                                       attributes=need_fields))
+                return TABLE_WAS_UPDATED
+
+    def delete_object(self, py_object: object):
+        """
+            Удаление записи
+        :param py_object: объект записи
+        :return: статус удаления или контролируемая ошибка
+        """
+        need_fields: List[Tuple] = handler.convert_python_types_in_sqlite_types(attributes=tuple(vars(py_object).items()),
+                                                                                object_=True)
+        tables_list: tuple = self.get_tables()
+
+        for table in tables_list:
+
+            if set(row[1:] for row in self.get_table_info(table)) == set(need_fields):
+
+                try:
+                    self._execute(queries=util.check_having_in_table(table=table,
+                                                                     id_=py_object.id))
+                    id_: int = self._cursor.fetchone()[0]
+
+                except AttributeError:
+                    raise Exception(EXCEPTION_TEXT_NO_ID)
+                except (IndexError, TypeError):
+                    raise Exception(EXCEPTION_TEXT_NO_RECORD)
+
+                self._commit(queries=util.delete_object(table=table,
+                                                        row_id=id_))
+                return RECORD_WAS_DELETED
+
+        else:
+            raise Exception(EXCEPTION_TEXT_NO_TABLE_WITH_THESE_ATTRS)
+
+    def delete_class(self, py_class):
+        """
+            Удаление таблицы из БД
+        :param py_class: класс, по которому будет удалена бд
+        :return: статус удаления или контролируемая ошибка
+        """
+        need_fields: List[Tuple] = handler.convert_python_types_in_sqlite_types(attributes=tuple(vars(py_class).items()))
+        tables_list: tuple = self.get_tables()
+        title_class: str = py_class.__name__.lower()
+        if title_class not in tables_list:
+            raise Exception(EXCEPTION_TEXT_NO_TABLE_WITH_THESE_ATTRS)
+        else:
+            for table in tables_list:
+                if set(row[1:] for row in self.get_table_info(table)) == set(need_fields):
+                    self._commit(queries=util.delete_class(table=table))
+                    return TABLE_WAS_DELETED
+            else:
+                raise Exception(EXCEPTION_TEXT_NO_TABLE_WITH_THESE_ATTRS)
+
     def _set_constants(self):
         """
          Устанавливаем в классовые переменные инфу о базе и версии используемого API.
@@ -281,8 +391,11 @@ def main():
         # print(db.get_table_info('asdasdasdasd'))
         # print(db.find_class(Car))
         # print(db.find_classes_by((('color', 'str'), ('number', 'int'), ('title', 'str'))))
-        # db.find_hierarches()
-        # db.create_object('cat', '1')
+        # print(db.find_hierarches())
+        # db.create_object(Cat, '1')
         # db.create_objects('cat', 1, 2)
-
-
+        # db.save_object(Car("BLACK", 20, 'RENO'))
+        # db.save_object(Cat(22, 'test'))
+        # db.save_class(Cat)
+        # db.delete_object(Cat())
+        # db.delete_class(py_class=Car)
